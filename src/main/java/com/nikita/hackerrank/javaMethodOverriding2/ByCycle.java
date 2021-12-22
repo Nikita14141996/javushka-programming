101ccff7dfce826b7ec8782a3d2b8493dd039ed7
Nikita14141996 <nikolas141996@mail.ru>
2021-12-22 17:04:13 +0300

    HackerRank tasks

---
+++ src/main/java/com/nikita/hackerrank/javaMethodOverriding2/ByCycle.java
@@ -0,0 +1,11 @@
+package com.nikita.hackerrank.javaMethodOverriding2;
+/*
+
+https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem
+ */
+
+public class ByCycle {
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
---
+++ src/main/java/com/nikita/hackerrank/javaMethodOverriding2/Main.java
@@ -0,0 +1,11 @@
+package com.nikita.hackerrank.javaMethodOverriding2;
+/*
+
+https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
